110e4e7d778f724221620b7166f3bb09c68f9aa9
indiefernando <indie.fernando@Outlook.com>
2021-08-09 09:43:08 +1000

    Setting up project again

---
+++ src/com/goanna/BloodAlcoholContentCalculator.java
@@ -0,0 +1,4 @@
+package com.goanna;
+
+public class BloodAlcoholContentCalculator {
+}
---
+++ src/com/goanna/BloodAlcoholContentCalculatorTest.java
@@ -0,0 +1,7 @@
+package com.goanna;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BloodAlcoholContentCalculatorTest {
+
+}
