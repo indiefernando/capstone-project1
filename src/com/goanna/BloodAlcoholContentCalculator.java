7482226b9a48cb20419f70b5f715cd6140fddc54
indiefernando <indie.fernando@Outlook.com>
2021-08-09 09:51:31 +1000

    Creating gender variables as well as a scanner

--- src/com/goanna/BloodAlcoholContentCalculator.java
+++ src/com/goanna/BloodAlcoholContentCalculator.java
@@ -1,4 +1,15 @@
 package com.goanna;
 
+import java.util.Scanner;
+
 public class BloodAlcoholContentCalculator {
+
+    String male;
+    String input = "Male";
+
+    String female;
+    String inp = "Female";
+
+    Scanner scan = new Scanner(System.in);
+
 }
