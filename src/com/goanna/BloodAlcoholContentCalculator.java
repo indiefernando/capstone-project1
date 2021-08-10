0e3893f459bf5dcaefa37a026cd7213407f9497c
indiefernando <indie.fernando@Outlook.com>
2021-08-10 13:36:55 +1000

    Extracting my if or else statement into a method in order to test it

--- src/com/goanna/BloodAlcoholContentCalculator.java
+++ src/com/goanna/BloodAlcoholContentCalculator.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class BloodAlcoholContentCalculator {
     public static void main(String[] args) {
 
+        System.out.println("Welcome to the Blood Alcohol Content calculator!");
+
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Please enter your gender ('Male' or 'Female'): ");
         String input = scan.next();
 
+        extracted(scan, input);
+
+        System.out.println(" ");
+        System.out.println("Please note that the legal alcohol limit for learner and provisional license holders is zero. " +
+                "The legal limit for full license holders is 0.05.");
+    }
+
+    private static void extracted(Scanner scan, String input) {
         if(input.equalsIgnoreCase("Male")){
 
             System.out.println("Weight in kilograms:");
@@ -44,6 +54,7 @@ public class BloodAlcoholContentCalculator {
             double bloodAlcoholContent = ((10 * standardDrinks) - (7.5 * hoursDrinking)) / (5.5 * weight);
             System.out.println(bloodAlcoholContent);
 
+            System.out.println(" ");
             System.out.println("Hours until Blood Alcohol content is back to zero: ");
             double hoursUntilZero = bloodAlcoholContent / 0.015;
             System.out.println(hoursUntilZero);
--- src/com/goanna/BloodAlcoholContentCalculatorTest.java
+++ src/com/goanna/BloodAlcoholContentCalculatorTest.java
@@ -1,5 +1,10 @@
 package com.goanna;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BloodAlcoholContentCalculatorTest {
