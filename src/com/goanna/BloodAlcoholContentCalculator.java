80041cb273de3218e22afd0491365f29d84de55e
indiefernando <indie.fernando@Outlook.com>
2021-08-10 14:38:51 +1000

    First test that tests if the BAC formula is correctly used for females

--- src/com/goanna/BloodAlcoholContentCalculator.java
+++ src/com/goanna/BloodAlcoholContentCalculator.java
@@ -12,56 +12,43 @@ public class BloodAlcoholContentCalculator {
         System.out.println("Please enter your gender ('Male' or 'Female'): ");
         String input = scan.next();
 
-        extracted(scan, input);
+        if(input.equalsIgnoreCase("Male")|| input.equalsIgnoreCase("Female")){
+
+            System.out.println("Weight in kilograms:");
+            double weight = scan.nextDouble();
+
+            System.out.println("Amount of standard drinks consumed:");
+            double standardDrinks = scan.nextDouble();
+
+            System.out.println("Amount of hours since your first drink:");
+            double hoursDrinking = scan.nextDouble();
+
+            System.out.println("Your current Blood Alcohol Content is at: ");
+            double bloodAlcoholContent = getBloodAlcoholContent(input, weight, standardDrinks, hoursDrinking);
+            System.out.println(bloodAlcoholContent);
+
+            System.out.println("Hours until Blood Alcohol content is back to zero: ");
+            double hoursUntilZero = bloodAlcoholContent / 0.015;
+            System.out.println(hoursUntilZero);
+
+
+        } else {
+            System.out.println("Invalid input for gender. Please enter 'Male' or 'Female'");
+
+        }
 
         System.out.println(" ");
         System.out.println("Please note that the legal alcohol limit for learner and provisional license holders is zero. " +
                 "The legal limit for full license holders is 0.05.");
     }
 
-    private static void extracted(Scanner scan, String input) {
-        if(input.equalsIgnoreCase("Male")){
+    public static double getBloodAlcoholContent( String gender, double weight, double standardDrinks, double hoursDrinking) {
 
-            System.out.println("Weight in kilograms:");
-            double weight = scan.nextDouble();
-
-            System.out.println("Amount of standard drinks consumed:");
-            double standardDrinks = scan.nextDouble();
-
-            System.out.println("Amount of hours since your first drink:");
-            double hoursDrinking = scan.nextDouble();
-
-            System.out.println("Your current Blood Alcohol Content is at: ");
-            double bloodAlcoholContent = ((10 * standardDrinks) - (7.5 * hoursDrinking)) / (6.8 * weight);
-            System.out.println(bloodAlcoholContent);
-
-            System.out.println("Hours until Blood Alcohol content is back to zero: ");
-            double hoursUntilZero = bloodAlcoholContent / 0.015;
-            System.out.println(hoursUntilZero);
-
-        } else if (input.equalsIgnoreCase("Female")) {
-
-            System.out.println("Weight in kilograms:");
-            double weight = scan.nextDouble();
-
-            System.out.println("Amount of standard drinks consumed:");
-            double standardDrinks = scan.nextDouble();
-
-            System.out.println("Amount of hours since your first drink:");
-            double hoursDrinking = scan.nextDouble();
-
-            System.out.println("Your current Blood Alcohol Content is at: ");
-            double bloodAlcoholContent = ((10 * standardDrinks) - (7.5 * hoursDrinking)) / (5.5 * weight);
-            System.out.println(bloodAlcoholContent);
-
-            System.out.println(" ");
-            System.out.println("Hours until Blood Alcohol content is back to zero: ");
-            double hoursUntilZero = bloodAlcoholContent / 0.015;
-            System.out.println(hoursUntilZero);
-
+        if (gender.equalsIgnoreCase("Male")) {
+            return ((10 * standardDrinks) - (7.5 * hoursDrinking)) / (6.8 * weight);
         } else {
-            System.out.println("Invalid input for gender. Please enter 'Male' or 'Female'");
-
+            return ((10 * standardDrinks) - (7.5 * hoursDrinking)) / (5.5 * weight);
         }
+
     }
 }
--- src/com/goanna/BloodAlcoholContentCalculatorTest.java
+++ src/com/goanna/BloodAlcoholContentCalculatorTest.java
@@ -1,13 +1,21 @@
 package com.goanna;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.Scanner;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BloodAlcoholContentCalculatorTest {
+
+  @Test
+    public void getBloodAlcoholContentForFemale() {
+    String gender = "Female";
+    double weight = 60;
+    double standardDrinks = 2.0;
+    double hoursDrinking = 1.0;
+
+    double bloodAlcoholContent = BloodAlcoholContentCalculator.getBloodAlcoholContent(gender, weight, standardDrinks, hoursDrinking);
+
+    assertEquals(0.03787878787878788, bloodAlcoholContent);
+  }
 
 
 }
