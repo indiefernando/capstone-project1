b67148807451b3e881e1286a009b3882d33e6aab
indiefernando <indie.fernando@Outlook.com>
2021-08-10 14:41:32 +1000

    Second test that tests that the BAC formula for males works correctly

--- src/com/goanna/BloodAlcoholContentCalculatorTest.java
+++ src/com/goanna/BloodAlcoholContentCalculatorTest.java
@@ -17,5 +17,16 @@ class BloodAlcoholContentCalculatorTest {
     assertEquals(0.03787878787878788, bloodAlcoholContent);
   }
 
+  @Test
+  public void getBloodAlcoholContentForMale() {
+    String gender = "Male";
+    double weight = 60;
+    double standardDrinks = 2.0;
+    double hoursDrinking = 1.0;
+
+    double bloodAlcoholContent = BloodAlcoholContentCalculator.getBloodAlcoholContent(gender, weight, standardDrinks, hoursDrinking);
+
+    assertEquals(0.030637254901960783, bloodAlcoholContent);
+  }
 
 }
